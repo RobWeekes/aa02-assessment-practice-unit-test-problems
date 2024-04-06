d425aa3dc83def91fda81358cac11fde4c9c4c78
robweekes <weekes.robert@gmail.com>
2024-04-06 11:35:21 -0700

    done with 1

--- problems/01-key-adder-unique-val.js
+++ problems/01-key-adder-unique-val.js
@@ -13,9 +13,36 @@ keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange
 console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
 ***********************************************************************/
 
-function keyAdderUniqueVal(object, key, value) {
-	// Your code here 
+
+function keyAdderUniqueVal(object, newKey, value) {
+	console.log(object);
+
+	for(key in object) {
+		console.log(`current key value: ${object[key]}`);
+
+		// screen out any key/values with matching values, so do nothing & return same object
+		if(value === (object[key])) {
+			console.log(`matching key value: ${object[key]}`);
+			console.log(key);
+			console.log(object[key]);
+			console.log(`${value} is already a value in obj, do nothing`);
+			console.log(object);
+			return object;
+		}
+	}
+
+	// if all key values dont match the input 'value', add the pair outside the 'for key' loop
+	object[newKey] = value;
+	console.log(object);
+	return object;
 }
+
+let cat = { name: 'Willie', color: 'orange' };
+keyAdderUniqueVal(cat, "toy", "yarn"); // => {name: "Willie", color: "orange", toy: "yarn"}
+console.log(cat);
+keyAdderUniqueVal(cat, "fruit", "orange"); // => {name: "Willie", color: "orange", toy: "yarn"}
+console.log(cat); // { name: "Willie", color: "orange", toy: "yarn" }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyAdderUniqueVal;
